27942355df17d46f07a0d3ebdb38bcd14393f708
natejmaster@gmail.com <natejmaster@gmail.com>
2023-09-19 20:23:37 -0500

    Added code for all three models

---
+++ models/Book.js
@@ -0,0 +1,55 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Book extends Model {}
+
+Book.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    authors: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    genre: {
+      type: DataTyples.STRING,
+      allowNull: false,
+    },
+    average_rating: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    isbn: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'book',
+  }
+);
+
+module.exports = Book;
