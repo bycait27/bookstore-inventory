c5226b99d3212655b315a85ccc34a9506c7f74a9
natejmaster@gmail.com <natejmaster@gmail.com>
2023-10-02 18:53:35 -0500

    Added logout feature

---
+++ public/js/logout.js
@@ -0,0 +1,22 @@
+const logoutHandler = async () => {
+    try {
+      // Send a GET request to the logout endpoint
+      const response = await fetch('/api/users/logout', {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        // If successful, redirect the browser to the login page or another page
+        document.location.replace('/');
+      } else {
+        alert('Failed to log out.');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      alert('An error occurred while logging out.');
+    }
+  };
+  
+  // Add an event listener to the logout link or button
+  document.querySelector('#logout-link').addEventListener('click', logoutHandler);
